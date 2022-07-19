dc4b674256dff3b3149200734a6fcc33135e3d48
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-07-19 21:18:41 +0100

    Added more styles and Added Some Project

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,32 +1,33 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Img = styled.img`
-  width:100%;
-  height:100%;
+  width: 100%;
+  height: 100%;
   object-fit: cover;
   overflow: hidden;
-`
+`;
 
 export const GridContainer = styled.section`
-display: grid;
-grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-padding: 3rem;
-place-items: center;
-column-gap: 2rem;
-row-gap: 3rem;
-@media ${(props) => props.theme.breakpoints.sm} {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  padding-bottom: 0;
-}
-
-`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  padding: 3rem;
+  place-items: center;
+  column-gap: 2rem;
+  row-gap: 3rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    display: flex;
+    flex-direction: column;
+    column-gap: 7rem;
+    padding: 2rem;
+    margin-bottom: 0px;
+  }
+`;
 export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
+
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
@@ -34,17 +35,16 @@ export const BlogCard = styled.div`
 export const TitleContent = styled.div`
   text-align: center;
   z-index: 20;
+  margin-top: 10px;
   width: 100%;
-
 `;
-
 
 export const HeaderThree = styled.h3`
   font-weight: 500;
   letter-spacing: 2px;
   color: #9cc9e3;
-  padding: .5rem 0;
-  font-size: ${(props) => props.title ? '3rem' : '2rem'};
+  padding: 0.5rem 0;
+  font-size: ${(props) => (props.title ? "3rem" : "2rem")};
 `;
 
 export const Hr = styled.hr`
@@ -59,26 +59,23 @@ export const Intro = styled.div`
   width: 170px;
   margin: 0 auto;
   color: #dce3e7;
-  font-family: 'Droid Serif', serif;
+  font-family: "Droid Serif", serif;
   font-size: 13px;
   font-style: italic;
   line-height: 18px;
 `;
 
-
 export const CardInfo = styled.p`
   width: 100%;
-  padding: 0 50px;
+  padding: 3%;
   color: #e4e6e7;
   font-style: 2rem;
   line-height: 24px;
   text-align: justify;
   @media ${(props) => props.theme.breakpoints.sm} {
-    padding:.3rem
-  
-}
+    padding: 0.3rem;
+  }
 `;
-
 
 export const UtilityList = styled.ul`
   list-style-type: none;
@@ -89,24 +86,28 @@ export const UtilityList = styled.ul`
 `;
 
 export const ExternalLinks = styled.a`
-color:#d4c0c0;
-font-size: 1.6rem;
-padding:1rem 1.5rem;
-background: #6b3030;
-border-radius: 15px;
-transition: 0.5s;
-&:hover{
-  background: #801414;
-
-}
+  color: #d4c0c0;
+  font-size: 1.6rem;
+  padding: 1rem 1.5rem;
+  background: #6b3030;
+  border-radius: 15px;
+  transition: 0.5s;
+  &:hover {
+    background: #801414;
+  }
 `;
 
 export const TagList = styled.ul`
-display: flex;
-justify-content: space-around;
-padding: 2rem;
-`
+  display: flex;
+  justify-content: space-around;
+  padding: 2rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 40px;
+  }
+`;
 export const Tag = styled.li`
-color: #d8bfbf;
-font-size: 1.5rem;
-`
+  color: #d8bfbf;
+  font-size: 1.5rem;
+`;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,40 +5,40 @@ export const projects = [
       "This project was made when I started out. It was my first e-commerce project with the basic Functionality you will see on an e-commerce website. ",
     image: "/images/1.jpg",
     tags: ["Bootstrap", "Javascript", "Express", "Node"],
-    source: "https://google.com",
-    visit: "https://google.com",
+    source: "https://github.com/Zuxxypml/Zuxxy-mart",
+    visit: "https://zuxxy-mart.herokuapp.com/",
     id: 0,
   },
   {
-    title: "E-Commerce",
+    title: "Simon Game",
     description:
-      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+      "Another of my Starting out Application. The Popular Simon Game Replica Made with Javascript and Jquery",
     image: "/images/2.jpg",
-    tags: ["React", "JavaScript"],
-    source: "https://google.com",
-    visit: "https://google.com",
+    tags: ["JavaScript", "Jquery", "Netlify"],
+    source: "https://github.com/Zuxxypml/Simon-game",
+    visit: "https://simon-game-by-zuxxy.netlify.app/",
     id: 1,
   },
-  {
-    title: "WebRTC App",
-    description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: "/images/3.jpg",
-    tags: ["React", "WebRTC"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 2,
-  },
-  {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/images/4.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 3,
-  },
+  // {
+  //   title: "WebRTC App",
+  //   description:
+  //     "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+  //   image: "/images/3.jpg",
+  //   tags: ["React", "WebRTC"],
+  //   source: "https://google.com",
+  //   visit: "https://google.com",
+  //   id: 2,
+  // },
+  // {
+  //   title: "Unichat",
+  //   description:
+  //     "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: "/images/4.jpg",
+  //   tags: ["React", "ChatEngine", "Firebase"],
+  //   source: "https://google.com",
+  //   visit: "https://google.com",
+  //   id: 3,
+  // },
 ];
 
 export const TimeLineData = [
